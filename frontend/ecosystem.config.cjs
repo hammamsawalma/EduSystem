0aca7cced7efed04235edf6799e871be9dbea47f
HichOoMa <haitham.benhammouda@gmail.com>
2025-08-23 20:46:52 +0100

    fix: update frontend script to use 'npm run preview' instead of 'npm run build && npm start'

--- frontend/ecosystem.config.cjs
+++ frontend/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'frontend',
-      script: 'npm run build && npm start',
+      script: 'npm run preview',
       watch: false,
       max_memory_restart: '1G',
       log_date_format: 'YYYY-MM-DD HH:mm Z',
