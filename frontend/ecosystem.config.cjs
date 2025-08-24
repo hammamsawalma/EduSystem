6b31028a5d45e3263d68fd1f0871d6b4acee23b4
HichOoMa <haitham.benhammouda@gmail.com>
2025-08-24 11:46:16 +0100

    fix: update ecosystem configuration to run build before preview

--- frontend/ecosystem.config.cjs
+++ frontend/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'frontend',
-      script: 'npm run preview',
+      script: 'npm run build & npm run preview',
       watch: false,
       max_memory_restart: '1G',
       log_date_format: 'YYYY-MM-DD HH:mm Z',
